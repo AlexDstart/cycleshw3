f7acad54468437d99fd16b167c2e0334e3ae085b
AlexWorkPC <demyanalex@gmail.com>
2022-11-27 21:58:21 +0300

    задание 3-2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 3-1");
-        int salary=29000;
+        int contribution=29000;
         int total =0;
         for(int i = 1; i<=12; i++){
-            total=total+salary;
-            System.out.println(" месяц "+ i+" сумма накоплений " + total);
+            total+=contribution;
+            System.out.println(" месяц " + i + " сумма накоплений " + total + " рублей ");
         }
+
+        //task3-2
+        System.out.println(" Задание 3-2 ");
+        double contributionWithPercent=0;
+        double percent=0.01;
+        for (int i = 1; i <=12 ; i++) {
+            contributionWithPercent=(contributionWithPercent+contribution)*(1+percent);
+            System.out.println(" Месяц " + i + " сумма накоплений равна " + contributionWithPercent + " рублей ");
+
+        }
+
     }
 }
